cd3eb37ddfb3b11d2cf12ce906c71ef167007d29
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-02-03 11:54:27 -0600

    home work 5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Error404 from './pages/Error404'
-import PreJunior from './pages/PreJunior'
-import Junior from './pages/Junior'
-import JuniorPlus from './pages/JuniorPlus'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Error404 from "./pages/Error404";
+import PreJunior from "./pages/PreJunior";
+import Junior from "./pages/Junior";
+import JuniorPlus from "./pages/JuniorPlus";
 
 export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
-}
+  PRE_JUNIOR: "/pre-junior",
+  JUNIOR: "/junior",
+  JUNIOR_PLUS: "/junior-plus",
+};
 
 function Pages() {
-    return (
-        <div>
-            {/*Routes выбирает первый подходящий роут*/}
-            <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+  return (
+    <div>
+      {/*Routes выбирает первый подходящий роут*/}
+      <Routes>
+        {/*роутинг будут писать студенты*/}
+        {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+        <Route path={"/"} element={<PreJunior />} />
+        {/*<Route ...*/}
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+        {/*роуты для /pre-junior, /junior, /junior-plus*/}
+        <Route path={"/pre-junior"} element={<JuniorPlus />} />
+        <Route path={"/junior"} element={<Junior />} />
+        <Route path={"/junior-plus"} element={<JuniorPlus />} />
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
-            </Routes>
-        </div>
-    )
+        {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+        <Route path={"*"} element={<Error404 />} />
+        {/*<Route ...*/}
+      </Routes>
+    </div>
+  );
 }
 
-export default Pages
+export default Pages;
