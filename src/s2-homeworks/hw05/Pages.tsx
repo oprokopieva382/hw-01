1302fd695e60faa87c57d873fe949644d2828b81
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-02-03 12:00:34 -0600

    error component fixed

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -27,7 +27,7 @@ function Pages() {
         <Route path={"/junior-plus"} element={<JuniorPlus />} />
 
         {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-        <Route path={"*"} element={<Error404 />} />
+        <Route path={"/*"} element={<Error404 />} />
         {/*<Route ...*/}
       </Routes>
     </div>
