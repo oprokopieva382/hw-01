010b7544069b38c69ebe308e77a1f3bc9be73af2
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-02-25 00:52:08 -0600

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,38 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      if (action.payload === "up") {
+        return [...state].sort((a, b) => {
+          if (a.name > b.name) {
+            return 1;
+          }
+          return -1;
+        });
+      }
+      if (action.payload === "down") {
+        return [...state].sort((a, b) => {
+          if (a.name < b.name) {
+            return 1;
+          }
+          return -1;
+        });
+      }
+      return state;
     }
-}
+    case "check": {
+      return state.filter((el) => el.age > action.payload); // need to fix
+    }
+    default:
+      return state;
+  }
+};
